377fc46ba82e82499ddda627ae5553718b208e50
Minh Hoai <minhhoai@cs.stonybrook.edu>
2015-07-14 14:33:50 -0400

    Update the version number
    MHLFuncs 4.1

--- ml_version.m
+++ ml_version.m
@@ -2,6 +2,6 @@ function [version, versionDate] = ml_version()
 % By: Minh Hoai Nguyen (minhhoai@cs.stonybrook.edu)
 % Created: 12-Jul-2015
 % Last modified: 12-Jul-2015
-    version = '4.0';
-    versionDate = '12-Jul-2015';
+    version = '4.1';
+    versionDate = '14-Jul-2015';
     
